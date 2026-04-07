ecb880fe5a5cb2fcac27313e8a825702290a7504
Adam Robson <adam.robson.snb@gmail.com>
2026-04-07 21:40:17 +0100

    01/04/2026

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.snb.inspect"
         minSdk = 31
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
